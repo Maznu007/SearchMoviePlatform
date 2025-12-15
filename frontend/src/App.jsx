0b75d02a99bed0a5e93a97000d9ac446fe349e5d
Maznu007 <hasnatrafiuddinmunif@gmail.com>
2025-12-15 18:57:48 +0600

    start

---
+++ frontend/src/App.jsx
@@ -0,0 +1,72 @@
+
+// import './App.css'
+
+// function App() {
+
+//   return (
+//     <>
+//       <div>
+//         <p>hello</p>
+//         <Text />
+//         <Hello display="hello (display)" />
+//       </div>
+//     </>
+//   )
+// }
+
+// function Text(){
+//   return(
+//     <div><p>hello2</p></div>
+//   )
+// }
+
+// function Hello({display}){
+//   return(
+//     <div><p>{display}</p></div>
+//   )
+// }
+
+// export default App
+// import './App.css'
+// import MovieCard from './components/MovieCard';
+
+// function App() {
+//   return (
+//     <>
+//     <MovieCard movie = {{title: "tim" , release_date: "2222"}} />
+
+//     </>
+//   )
+// }
+
+// export default App
+
+
+// import './App.css'
+// import MovieCard from './components/MovieCard';
+
+// function App() {
+//   const movieNumber = 2;
+//   return (
+//     <>
+//     {movieNumber === 1 ? <MovieCard movie = {{title: "tim" , release_date: "2222"}} /> : <MovieCard movie = {{title: "dim" , release_date: "2223"}} />}
+
+//     </>
+//   )
+// }
+
+// export default App
+
+import './App.css'
+import Home from './pages/Home.jsx';
+
+function App() {
+  return (
+    <>
+    <Home />
+
+    </>
+  )
+}
+
+export default App
